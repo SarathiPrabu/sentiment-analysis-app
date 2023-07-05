17c5673959290c23710a7922dda6c86bb3330212
SarathiPrabu <124449777+SarathiPrabu@users.noreply.github.com>
2023-07-05 15:14:44 -0400

    Updated app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
 import streamlit as st
 from transformers import pipeline
+import torch
+import random
 
-# Create a sentiment analysis pipeline using a specific model
-nlp_pipeline = pipeline(
-    "sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english"
-)
+# Check if GPU is available
+if torch.cuda.is_available():
+    device = 0
+else:
+    # If GPU is not available, use CPU
+    device = torch.device("cpu")
+
+model_name = "facebook/bart-large-mnli"
+nlp_pipeline = pipeline("zero-shot-classification", model=model_name, device=device)
+
+# Random text for pre population. If we want to enter custom texts, this can be commented
+random_reviews = [
+    "This movie was fantastic! I loved every minute of it.",
+    "The food at this restaurant is awful. I would not recommend it.",
+    "I had a great experience at this hotel. The staff was friendly and helpful.",
+    "The service was slow and the prices were too high. I was disappointed.",
+    "I absolutely adore this book. It's a must-read for everyone.",
+]
 
 # Streamlit application title
-st.title("Sentiment Analysis")
+st.title("Sentiment Analysis - Hugging Space")
 
 # Text input box
-text_input = st.text_area("Enter text for sentiment analysis")
+text_input = st.text_area(
+    "Enter text for sentiment analysis", value=random.choice(random_reviews)
+)
+text_labels = ["Positive", "Negative", "Neutral"]
 
 # Analyze button
 if st.button("Analyze"):
     if text_input:
-        # Perform sentiment analysis on the input text
-        sentiment = nlp_pipeline(text_input)
+        results = nlp_pipeline(text_input, text_labels)
+        sentiment = results["labels"][0]
+        confidence = results["scores"][0]
 
-        # Get the sentiment label and score
-        label = sentiment[0]["label"]
-        score = sentiment[0]["score"]
+        # Display the sentiment analysis results
+        st.write("Sentiment:", sentiment)
+        st.write("Confidence:", round(confidence * 100, 2), "%")
 
-        # Display the sentiment analysis result
-        st.write("Sentiment:", label)
-        st.write("Confidence:", score)
-
-        # Map sentiment label to positive, negative, or neutral
-        sentiment_mapping = {
-            "NEGATIVE": "Negative",
-            "NEUTRAL": "Neutral",
-            "POSITIVE": "Positive",
-        }
-        mapped_sentiment = sentiment_mapping.get(label)
-
-        # Display the mapped sentiment
-        st.write("Mapped Sentiment:", mapped_sentiment)
     else:
         st.warning("Please enter some text for analysis.")
