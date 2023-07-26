05643f5b96424c808a32ad43061727aa0ad4ac77
Sarathi Prabu Mohan <124449777+SarathiPrabu@users.noreply.github.com>
2023-07-26 14:32:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 import torch
-import random
+import pandas as pd
+from datasets import load_dataset
+
 
 # Check if GPU is available
 if torch.cuda.is_available():
@@ -10,37 +12,64 @@ else:
     # If GPU is not available, use CPU
     device = torch.device("cpu")
 
-model_name = "facebook/bart-large-mnli"
-nlp_pipeline = pipeline("zero-shot-classification", model=model_name, device=device)
 
-# Random text for pre population. If we want to enter custom texts, this can be commented
-random_reviews = [
-    "This movie was fantastic! I loved every minute of it.",
-    "The food at this restaurant is awful. I would not recommend it.",
-    "I had a great experience at this hotel. The staff was friendly and helpful.",
-    "The service was slow and the prices were too high. I was disappointed.",
-    "I absolutely adore this book. It's a must-read for everyone.",
-]
+# model_name = "facebook/bart-large-mnli"
+# nlp_pipeline = pipeline("zero-shot-classification", model=model_name, device=device)
+# st.title("USPTO Patentability Score")
 
-# Streamlit application title
-st.title("Sentiment Analysis - Hugging Space")
 
-# Text input box
-text_input = st.text_area(
-    "Enter text for sentiment analysis", value=random.choice(random_reviews)
+@st.cache(allow_output_mutation=True)
+def load_data():
+    dataset_dict = load_dataset(
+        "HUPD/hupd",
+        name="sample",
+        data_files="https://huggingface.co/datasets/HUPD/hupd/blob/main/hupd_metadata_2022-02-22.feather",
+        icpr_label=None,
+        train_filing_start_date="2016-01-01",
+        train_filing_end_date="2016-01-31",
+        val_filing_start_date="2016-02-22",
+        val_filing_end_date="2016-02-29",
+    )
+    df = pd.DataFrame.from_dict(dataset_dict["train"])
+    df = df[["patent_number", "title", "decision", "abstract", "claims", "filing_date"]]
+    return df
+
+
+df = load_data()
+patent_info = df[["patent_number", "title"]]
+patent_info["patent_info"] = patent_info.apply(
+    lambda row: f"{row['patent_number']} - {row['title']}", axis=1
 )
-text_labels = ["Positive", "Negative", "Neutral"]
 
-# Analyze button
-if st.button("Analyze"):
-    if text_input:
-        results = nlp_pipeline(text_input, text_labels)
-        sentiment = results["labels"][0]
-        confidence = results["scores"][0]
+patent_number = patent_info["patent_info"].drop_duplicates().reset_index(drop=True)
 
-        # Display the sentiment analysis results
-        st.write("Sentiment:", sentiment)
-        st.write("Confidence:", round(confidence * 100, 2), "%")
+st.sidebar.subheader("Select the Patent:")
+make_choice = st.sidebar.selectbox("", patent_number)
 
-    else:
-        st.warning("Please enter some text for analysis.")
+with st.form("patent-form"):
+    submitted = st.form_submit_button(label="Submit")
+
+if submitted:
+    selected_patent_number = make_choice.split(" - ")[0]
+    st.subheader(f"Patent Application Number: {selected_patent_number}")
+
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+    decision = df["decision"].loc[df["patent_number"] == selected_patent_number]
+    X_train = decision.to_string(index=False)
+    results = classifier(X_train, truncation=True)
+
+    for result in results:
+        score = result["score"]
+        st.write("The Patentability Score is:", score)
+
+    abstract = df["abstract"].loc[df["patent_number"] == selected_patent_number]
+    st.subheader("Abstract:")
+    st.info(abstract.iloc[0])
+
+    claims = df["claims"].loc[df["patent_number"] == selected_patent_number]
+    st.subheader("Claim:")
+    st.info(claims.iloc[0])
